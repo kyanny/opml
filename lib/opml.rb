e5c3d5ec41022c600b333cbec8b7ff7ea10c7410
Yamagishi Kazutoshi <ykzts@desire.sh>
2013-03-16 05:15:00 +0900

    Fixed Opml::Outline#flatten

--- lib/opml.rb
+++ lib/opml.rb
@@ -21,7 +21,7 @@ class Opml
     end
 
     def flatten
-      @flatten ||= @outlines.map(&:flatten).unshift(self)
+      @flatten ||= @outlines.map(&:flatten).flatten.unshift(self)
     end
 
     def to_s
